dd374314c2219fb2455b50b7eb02101471e2b722
gavinswan <gavtswan@gmail.com>
2021-03-06 22:32:32 +0000

    Make changes to schema. Delete email

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,28 +1,28 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, is_admin) VALUES ('Elon musk', 'emusk@spacex.com', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Elon musk', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Baloo', 'barenecessities@jungle.in', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Baloo', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Jean Batten', 'nzflyer@kiwi.com', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Jean Batten', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Han Solo', 'flyingsolo@milleniumfalcon.com', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Han Solo', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Pete Mitchell', 'maverick@usaf.gov', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Pete Mitchell', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Raisa Aronova', 'nachthexen@ussr.su', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Raisa Aronova', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Samuel L. Jackson', 'iliketoyell@hollwood.com', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Samuel L. Jackson', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Sully Sullenberger', 'ssullenberger@usairways.com', TRUE);
+INSERT INTO users (name, is_admin) VALUES ('Sully Sullenberger', TRUE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Hikaru Sulu', 'takei@enterprise.us', FALSE);
+INSERT INTO users (name, is_admin) VALUES ('Hikaru Sulu', FALSE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Virgil Tracy', 'thunderbird2@ir.org', FALSE);
+INSERT INTO users (name, is_admin) VALUES ('Virgil Tracy', FALSE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Fox McCloud', 'arwing@greatfox.org', FALSE);
+INSERT INTO users (name, is_admin) VALUES ('Fox McCloud', FALSE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Frank Abignail', 'johndoe@panamairways.com', FALSE);
+INSERT INTO users (name, is_admin) VALUES ('Frank Abignail', FALSE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Wright Brothers', 'aviation@history.com', FALSE);
+INSERT INTO users (name, is_admin) VALUES ('Wright Brothers', FALSE);
 
-INSERT INTO users (name, email, is_admin) VALUES ('Amelia Earhart', 'solo@atlantic.us', FALSE);
+INSERT INTO users (name, is_admin) VALUES ('Amelia Earhart', FALSE);
