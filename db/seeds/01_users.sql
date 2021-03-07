a6094b81aed82ce0b89e45cfbe7b5c1ac9c59597
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-07 18:11:19 +0000

    updated users seed to have seller_id instead of admin

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,28 +1,28 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, is_admin) VALUES ('Elon musk', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Elon musk', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Baloo', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Baloo', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Jean Batten', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Jean Batten', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Han Solo', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Han Solo', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Pete Mitchell', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Pete Mitchell', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Raisa Aronova', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Raisa Aronova', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Samuel L. Jackson', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Samuel L. Jackson', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Sully Sullenberger', TRUE);
+INSERT INTO users (name, is_seller) VALUES ('Sully Sullenberger', TRUE);
 
-INSERT INTO users (name, is_admin) VALUES ('Hikaru Sulu', FALSE);
+INSERT INTO users (name, is_seller) VALUES ('Hikaru Sulu', FALSE);
 
-INSERT INTO users (name, is_admin) VALUES ('Virgil Tracy', FALSE);
+INSERT INTO users (name, is_seller) VALUES ('Virgil Tracy', FALSE);
 
-INSERT INTO users (name, is_admin) VALUES ('Fox McCloud', FALSE);
+INSERT INTO users (name, is_seller) VALUES ('Fox McCloud', FALSE);
 
-INSERT INTO users (name, is_admin) VALUES ('Frank Abignail', FALSE);
+INSERT INTO users (name, is_seller) VALUES ('Frank Abignail', FALSE);
 
-INSERT INTO users (name, is_admin) VALUES ('Wright Brothers', FALSE);
+INSERT INTO users (name, is_seller) VALUES ('Wright Brothers', FALSE);
 
-INSERT INTO users (name, is_admin) VALUES ('Amelia Earhart', FALSE);
+INSERT INTO users (name, is_seller) VALUES ('Amelia Earhart', FALSE);
