1d44d955d40e3615698b5f4a7a8e0dbf03e73d99
gavinswan <gavtswan@gmail.com>
2021-03-11 03:13:49 +0000

    refactor code

--- public/scripts/createListing.js
+++ public/scripts/createListing.js
@@ -1,36 +1,48 @@
-const{ createListingElement } = require('./app')
-
 
 $(document).ready(function () {
-  $("#createlistinglink").on("submit", function (event) {
-    event.preventDefault();
-    let currentUser = $("#login-text").val();
-  $.ajax({
-    method: "GET",
-    url: "/api/create",
-    data: $(this).serialize(),
-  }).then(() => {
-      $("#listing").replaceWith('
+  $("#createlistingslink").on("click", function (event) {
+     event.preventDefault();
+     console.log(event)
+     $("#list").empty()
+     $("#page-title").replaceWith("<p>Create A Listing</p>")
+    return $("#list").prepend(`
+
       <div class="create-container">
-        <input name="price" type="text" class="" placeholder="seller ID"></input>
-        <input name="price" type="text" class="" placeholder="title"></input>
-        <input name="price" type="text" class="create-description" placeholder="description"></input>
-        <input name="price" type="text" class="create-price" placeholder="Price"></input>
-        <input name="price" type="text" class="create-year" placeholder="Year"></input>
-        <input name="make" type="text" class="create-make" placeholder="Make"></input>
-        <input name="model"type="text" class="create-model" placeholder="Model">
-        <button type="submit" class="button">Create Listing</button>
+      <form action="api/listings/" method="POST" id="createlisting">
+        <input name="seller_id" type="number" class="create-input" placeholder="Seller ID"></input>
+        <input name="title" type="text" class="create-input" placeholder="title"></input>
+        <input name="description" type="text" class="create-input" placeholder="Description"></input>
+        <input name="price" type="number" class="create-input" placeholder="Price"></input>
+        <input name="thumbnail_photo" type="text" class="create-input" placeholder="Photo"></input>
+        <input name="year" type="number" class="create-input" placeholder="Year"></input>
+        <input name="make" type="text" class="create-input" placeholder="Make"></input>
+        <input name="model"type="text" class="create-input" placeholder="Model"></input>
+
+          <button type="submit" class="button">Create Listing</button>
+        </form>
       </div>
-        ')
+      `)
+    })
+  }
+)
 
-      })
-})
-})
+// $(document).ready(function () {
+//   $("#createlisting").on("click", function(event) {
+//     let listingsData = $(".create-container").val()
+//   })
+//   return $.ajax({
+//     method: "POST",
+//     url: "/api/listings",
+//     data: listingsData.serialize()
+//   }).then((listings) => {
+//     console.log("listings", listings)
+//     addListings(listingsData, listings)
+//     console.log("listings after", listings)
 
+//   })
+// })
 
-
-
-{/* $(document).ready(function () {
+/* $(document).ready(function () {
   $("#createlistinglink").on("submit", function (event) {
     event.preventDefault();
     let currentUser = $("#login-text").val();
@@ -50,4 +62,4 @@ $(document).ready(function () {
     }
   })
 })
-}) */}
+}) */
