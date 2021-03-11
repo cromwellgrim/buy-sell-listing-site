d20356ab89407859a6fb560e68dd628acf2e9957
gavinswan <gavtswan@gmail.com>
2021-03-11 06:07:34 +0000

    Add routes to addListing

--- routes/addListing.js
+++ routes/addListing.js
@@ -1,5 +1,6 @@
+/* SQL inserts new listing into the listings table in the database */
 const addListings = function(db, listings) {
-  // console.log("listings in addListings", listings)
+
   const queryString = `
   INSERT INTO listings (seller_id, title, description, price, thumbnail_photo, year, make, model)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
@@ -7,7 +8,7 @@ const addListings = function(db, listings) {
   `;
 
   const queryParams = [listings.seller_id, listings.title, listings.description, listings.price, listings.thumbnail_photo, listings.year, listings.make, listings.model]
-  // console.log("listings post SQL", listings)
+
   return db.query(queryString, queryParams)
   .then((res) => {
     console.log("res in then", res)
